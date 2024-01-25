0cf61bd8ab9c88c6bb9bf9cf727b91387fda1a5d
Leiladany <leiladany16@gmail.com>
2024-01-25 18:40:44 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+import Chair from './Chair';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        {/* Chair */}
+        <Chair />
+
+        {/* Table */}
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ Table.js
@@ -0,0 +1,10 @@
+import {Text} from 'react-native';
+import React from 'react';
+
+class Table extends React.PureComponent {
+  render() {
+    return <Text>This is a table</Text>;
+  }
+}
+
+export default Table;
