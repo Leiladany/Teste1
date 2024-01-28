1ddfbceba4998b3af139d2b5cae42edc96a77d1b
Leiladany <leiladany16@gmail.com>
2024-01-28 21:21:00 +0000

    button component

--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Text, Image, View} from 'react-native';
+import {Text, Image, View, Pressable} from 'react-native';
 import {styles} from './styles';
+import Button from '../../../components/Button';
 
 const Splash = () => {
   return (
@@ -10,9 +11,17 @@ const Splash = () => {
         style={styles.images}
         source={require('../../../assets/splash.png')}
       />
+
+      <View style={styles.titleContainer} />
       <Text style={styles.title}> You'll find</Text>
       <Text style={[styles.title, styles.innerTitle]}>All you need</Text>
       <Text style={styles.title}>Here!</Text>
+
+      <Button title={'Sign Up'} />
+
+      <Pressable hitSlop={20}>
+        <Text style={styles.footerText}>Sign In</Text>
+      </Pressable>
     </View>
   );
 };
