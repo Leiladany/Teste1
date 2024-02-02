1bcb2c40c6ffc8db58824c1f9925c5fa2ab50cdc
Leiladany <leiladany16@gmail.com>
2024-02-02 17:18:32 +0000

    add categories list

---
+++ src/components/CategoryBox/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Image, Text, View, Pressable} from 'react-native';
+import {styles} from './styles';
+
+const CategoryBox = ({title, image, onPress}) => {
+  return (
+    <Pressable onPress={onPress} style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{uri: image}} />
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </Pressable>
+  );
+};
+
+export default React.memo(CategoryBox);
--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import {ScrollView, Text} from 'react-native';
+import {FlatList, ScrollView} from 'react-native';
 import {styles} from './styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Header from '../../../components/Header';
+import {categories} from '../../../data/categories';
+import CategoryBox from '../../../components/CategoryBox';
 
 const Home = () => {
+  const renderCategoryItem = ({item, index}) => {
+    console.log('item :>> ', item);
+
+    return <CategoryBox title={item?.title} image={item?.image} />;
+  };
   return (
     <SafeAreaView>
       <ScrollView style={styles.container}>
         <Header showSearch title="Find All You Need" />
-        <Text>Home</Text>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          style={styles.list}
+          horizontal
+          data={categories}
+          renderItem={renderCategoryItem}
+          keyExtractor={(item, index) => String(index)}
+        />
       </ScrollView>
     </SafeAreaView>
   );
