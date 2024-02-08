005e43bf07a062b337d7885f840936513c51bcd3
Leiladany <leiladany16@gmail.com>
2024-02-08 14:35:29 +0000

    register a new user with API call

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Alert} from 'react-native';
 import AuthHeader from '../../../components/AuthHeader';
 import Input from '../../../components/Input';
 import {styles} from './styles';
@@ -8,9 +8,11 @@ import Button from '../../../components/Button';
 import Seperator from '../../../components/Seperator';
 import GoogleLogin from '../../../components/GoogleLogin';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {request} from '../../../utils/request';
 
 const Signup = ({navigation}) => {
   const [checked, setChecked] = useState(false);
+  const [values, setValues] = useState({});
 
   const onSignIn = () => {
     navigation.navigate('Signin');
@@ -20,13 +22,73 @@ const Signup = ({navigation}) => {
     navigation.goBack();
   };
 
+  const onChange = (key, value) => {
+    setValues(v => ({...v, [key]: value}));
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (
+        !values?.fullName ||
+        !values?.email ||
+        !values?.password ||
+        !values?.confirmPassword
+      ) {
+        Alert.alert('All fields are required');
+        return;
+      }
+
+      if (values?.password !== values?.confirmPassword) {
+        Alert.alert('Passwords do not match');
+        return;
+      }
+
+      if (!checked) {
+        Alert.alert('Please agree to the terms');
+        return;
+      }
+
+      const response = await request({
+        url: '/user/register',
+        method: 'post',
+        data: values,
+      });
+      console.log('response :>> ', response);
+    } catch (error) {
+      console.log('error :>> ', error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <AuthHeader onBackPress={onBack} title="Sign Up" />
-        <Input label="Name" placeholder="John Doe" />
-        <Input label="Email" placeholder="example@gmail.com" />
-        <Input isPassword label="Password" placeholder="********" />
+        <Input
+          value={values.fullName}
+          onChangeText={v => onChange('fullName', v)}
+          label="Name"
+          placeholder="John Doe"
+        />
+        <Input
+          value={values.email}
+          onChangeText={v => onChange('email', v)}
+          label="Email"
+          placeholder="example@gmail.com"
+        />
+        <Input
+          value={values.password}
+          onChangeText={v => onChange('password', v)}
+          isPassword
+          label="Password"
+          placeholder="********"
+        />
+        <Input
+          value={values.confirmPassword}
+          onChangeText={v => onChange('confirmPassword', v)}
+          isPassword
+          label="confirmPassword"
+          placeholder="********"
+        />
 
         <View style={styles.agreeRow}>
           <Checkbox checked={checked} onCheck={setChecked} />
@@ -36,7 +98,7 @@ const Signup = ({navigation}) => {
           </Text>
         </View>
 
-        <Button style={styles.button} title="Sign Up" />
+        <Button onPress={onSubmit} style={styles.button} title="Sign Up" />
         <Seperator text="Or sign up with" />
         <GoogleLogin />
         <Text style={styles.footerText}>
